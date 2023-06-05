6d2787aaec710227ca34a1d02690ec61d4b52432
jester350 <jester350@gmail.com>
2023-06-05 14:42:20 +0100

    Delete disptext.js

--- disptext.js
+++
@@ -1,7 +0,0 @@
-function displayText() {
-    const textElement = document.getElementById("myText");
-    const text = "The times are local based on your own set timezone.";
-    textElement.textContent = text;
-}
-
-displayText();
